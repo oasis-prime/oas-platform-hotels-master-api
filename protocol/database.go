06674e88b39bf89b238473f694e2aa784e094d06
Sittha'macbook <sittha.raksasawat@gmail.com>
2022-09-23 06:47:10 +0700

    clean code

--- protocol/database.go
+++ protocol/database.go
@@ -2,7 +2,6 @@ package protocol
 
 import (
 	"fmt"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,15 +14,8 @@ func DBInit() {
 	instanceConnectionName := con.Database.Instance.Connection.Name
 	dbName := con.Database.Name
 
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd, instanceConnectionName, dbName)
 
-	var dbURI string
-	if isSet {
-		socketDir = "/cloudsql"
-		dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
-	} else {
-		dbURI = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd, instanceConnectionName, dbName)
-	}
 	var err error
 	db, err = gorm.Open(mysql.Open(dbURI), &gorm.Config{
 		Logger:                 logger.Default.LogMode(logger.Info),
