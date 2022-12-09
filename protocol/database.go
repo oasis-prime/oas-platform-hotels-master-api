a32c0708c9929f9420c93a1d1f5cd7f89a87d328
สิทธา รักษาสวัสดิ์ <sittha.raksasawat@gmail.com>
2022-12-09 22:56:43 +0700

    [🐛] fix bug

--- protocol/database.go
+++ protocol/database.go
@@ -2,6 +2,9 @@ package protocol
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,9 +19,19 @@ func DBInit() {
 
 	dbURI := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPwd, instanceConnectionName, dbName)
 
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,         // Disable color
+		},
+	)
+
 	var err error
 	db, err = gorm.Open(mysql.Open(dbURI), &gorm.Config{
-		Logger:                 logger.Default.LogMode(logger.Info),
+		Logger:                 newLogger,
 		SkipDefaultTransaction: true,
 		PrepareStmt:            true,
 	})
